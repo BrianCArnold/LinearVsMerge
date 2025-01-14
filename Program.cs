3f5f46dee9647bf51ab74a2a3fffe9caf9bdb46c
Brian C. Arnold <brian.arnold@spiderrock.net>
2025-01-14 12:47:38 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("These are the command line args.");
+
+for (int i = 0; i < args.Length; i++)
+{
+  Console.WriteLine($"- {args[i]}");
+
+}
