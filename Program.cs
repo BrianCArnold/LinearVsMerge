29a48a7c6da2af08e3ce4a5395d875769fbbce82
Brian C. Arnold <brian.arnold@spiderrock.net>
2025-01-14 12:47:38 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    List<string> TheListOfArgs = new();
+
+    Console.WriteLine($"Next we do some stuff with {nameof(TheListOfArgs)}");
+
+    foreach (var str in args)
+    {
+      TheListOfArgs.Add(str);
+    }
+
+    Console.WriteLine("And then we send the list of args to a function to process (print) them");
+
+    ProcessArgs(TheListOfArgs);
+  }
+
+  private static void ProcessArgs(IList<string> args)
+  {
+
+    Console.WriteLine("These are the command line args.");
+    for (int i = 0; i < args.Count(); i++)
+    {
+      Console.WriteLine($"- {args[i]}");
+
+    }
+  }
+}
